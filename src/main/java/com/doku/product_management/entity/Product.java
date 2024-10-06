0a793ae7362858def2bc65f496630ec64ff6173d
dewisnu <wisnuari4658@gmail.com>
2024-10-06 14:26:07 +0800

    feat: add entity Product

--- src/main/java/com/doku/product_management/entity/Product.java
+++ src/main/java/com/doku/product_management/entity/Product.java
@@ -7,10 +7,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     @Id
@@ -23,6 +29,7 @@ public class Product {
     @Min(value = 0, message = "Price must be a positive number")
     private BigDecimal price;
 
-
+    @Min(value = 0, message = "Quantity must be a positive integer")
+    private Integer quantity;
 
 }
